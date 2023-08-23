8e38b6f0774c2091478949144b21b392b7bb6fae
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-08-23 12:18:00 +0200

    feat(routes): types for routes base of react-navigation

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+import type { NavigationProp } from '@react-navigation/native';
+
+export type StackParamList = {
+  Home: undefined;
+  Settings: undefined;
+  Wingler: undefined;
+};
+
+export type StackNavigation = NavigationProp<StackParamList>;
