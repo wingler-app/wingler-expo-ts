77e10e2de57ae364b743fe6937cae23445231997
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-08-23 12:15:44 +0200

    feat(jest): mock for async storage

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,6 @@
 // Import Jest Native matchers
 import '@testing-library/jest-native/extend-expect';
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
