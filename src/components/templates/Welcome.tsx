d9b88f2c497fc09dea5529c996416a4a0e071fa5
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-08-31 12:02:46 +0200

    feat(welcome): made the landing page a bit nicer

--- src/components/templates/Welcome.tsx
+++ src/components/templates/Welcome.tsx
@@ -1,11 +1,47 @@
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Image, PermissionsAndroid, Platform, Text, View } from 'react-native';
 
-const Welcome = () => (
-  <View className="flex-1 items-center justify-center">
-    <Text>Hello World!</Text>
-    <Link href="/wingler">Wingler</Link>
-  </View>
-);
+const test = require('../../../assets/logo.png');
 
+const Welcome = () => {
+  const recordAudioRequest = async () => {
+    if (Platform.OS === 'android') {
+      // Android requires an explicit call to ask for permission
+      const granted = await PermissionsAndroid.request(
+        // @ts-ignoreh
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        {
+          title: 'Microphone Permission',
+          message: '[Permission explanation]',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    }
+    // iOS will automatically ask for permission
+    return true;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const hasPermission = await recordAudioRequest();
+      console.log(hasPermission);
+    })();
+
+    // return () => {
+    //   second
+    // }
+  }, []);
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Hello World!</Text>
+      <Image className="h-[92] w-[100]" source={test} />
+      <Link href="/wingler">Wingler</Link>
+    </View>
+  );
+};
 export { Welcome };
