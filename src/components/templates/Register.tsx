ecac1e618fb867a8c313428bbab32256b8f93e80
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-09-28 17:22:36 +0200

    feat(auth): new register and screen plus register/login template

---
+++ src/components/templates/Register.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const RegisterTemplate = () => {
+  return (
+    <View className="flex-1">
+      <Text>Register</Text>
+    </View>
+  );
+};
+
+export default RegisterTemplate;
