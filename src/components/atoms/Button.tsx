ef7d22db6b0b36a7f632dcc67a5ddb4e8236048d
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-10-03 11:04:35 +0200

    feat(atom): new button component

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,46 @@
+import { styled } from 'nativewind';
+import { useState } from 'react';
+import type { TextProps } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
+
+type ButtonProps = {
+  onPress: () => void;
+  title: string;
+  buttonStyle?: TextProps['style'];
+  disabled?: boolean;
+};
+
+function Button({ onPress, title, buttonStyle, disabled }: ButtonProps) {
+  const [isPressed, setIsPressed] = useState<boolean>(false);
+
+  const handleOnPress = () => {
+    if (disabled) return;
+    setIsPressed(false);
+    onPress();
+  };
+
+  return (
+    <View>
+      <Pressable
+        onPressIn={() => setIsPressed(true)}
+        onPressOut={() => setIsPressed(false)}
+        style={buttonStyle}
+        className={`mb-6 rounded-full bg-accent px-6 py-4 ${
+          isPressed && 'bg-accent-secondary'
+        }
+        ${disabled && 'bg-gray-400'} ${buttonStyle}`}
+        onPress={handleOnPress}
+      >
+        <Text className="text-center font-bold uppercase text-white dark:text-black">
+          {title}
+        </Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default styled(Button, {
+  props: {
+    buttonStyle: true,
+  },
+});
