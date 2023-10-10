89849851023040bf04e6fee27658e5416e4c5a2c
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-10-10 16:49:08 +0200

    feat(wingler-cloud): speach to text firebase openAI Q&A function

--- src/components/templates/WinglerBot.tsx
+++ src/components/templates/WinglerBot.tsx
@@ -1,11 +1,17 @@
 import { PicovoiceManager } from '@picovoice/picovoice-react-native';
 import type { RhinoInference } from '@picovoice/rhino-react-native';
+import Voice from '@react-native-voice/voice';
 import * as ExpoCrypto from 'expo-crypto';
-import { useEffect, useState } from 'react';
+import * as Speech from 'expo-speech';
+import { useEffect, useRef, useState } from 'react';
 import { View } from 'react-native';
 
-import type { BotQA, RhinoInferenceObject } from '@/types';
-import InferenceHandler, { prettyPrint } from '@/utils/inference';
+import type { RhinoInferenceObject } from '@/types';
+import { promptOpenAI } from '@/utils/handleQuestion';
+import InferenceHandler, {
+  prettyPrint,
+  speechOptions,
+} from '@/utils/inference';
 
 import Chat from '../organisms/chat';
 import ContextInfo from '../organisms/contextInfo';
@@ -22,35 +28,82 @@ const WinglerBot = () => {
   const [showInference, setShowInference] = useState<boolean>(false);
   const [showContextInfo, setShowContextInfo] = useState<boolean>(false);
   const [contextInfo, setContextInfo] = useState<string>('');
-  // const [picoVoice, setPicoVoice] = useState<PicovoiceManager | undefined>(
-  //   undefined,
-  // );
+  const [isSpeechToText, setIsSpeechToText] = useState<boolean>(false);
 
-  const wakeWordCallback = () => {
-    console.log('wingler');
+  const picovoiceManager = useRef<PicovoiceManager>();
+
+  const onSpeechStart = () => {
     setIsListening(true);
+    setIsSpeechToText(true);
   };
 
-  const inferenceCallback = (inference: RhinoInference) => {
+  const onSpeechEnd = () => {
     setIsListening(false);
-    setRhinoText(prettyPrint(inference));
-    const botQA: null | BotQA = InferenceHandler(inference);
-    if (botQA !== null) {
-      const obj: RhinoInferenceObject = {
-        botQA,
-        id: ExpoCrypto.getRandomBytes(16).toString(),
-      };
-      setHistory((previousState) => [...previousState, obj]);
-    } else {
-      console.log('Inference Text is null');
-    }
+    setIsSpeechToText(false);
+    picovoiceManager.current?.start();
   };
 
+  const onSpeechError = (e: any) => {
+    console.log('onSpeechError', e);
+    setIsListening(false);
+    setIsSpeechToText(false);
+    picovoiceManager.current?.start();
+  };
+
+  const onSpeechResults = (e: any) => {
+    console.log('onSpeechResults', e);
+    if (e === undefined || e.value === undefined) return;
+    console.log(e.value[0]);
+    (async () => {
+      if (e.value !== undefined) {
+        const last = e.value[0] as string;
+        const answer = await promptOpenAI(last);
+        Speech.speak(answer, speechOptions);
+      }
+    })();
+  };
   useEffect(() => {
-    let picovoiceManager: PicovoiceManager | undefined;
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+
+    const textToSpeech = async () => {
+      Voice.start('en-US');
+    };
+
+    const killPicoVoice = () => {
+      picovoiceManager.current?.stop();
+      textToSpeech();
+    };
+
+    const inferenceCallback = async (inference: RhinoInference) => {
+      setIsListening(false);
+      setRhinoText(prettyPrint(inference));
+
+      const botQA = await InferenceHandler(inference);
+
+      if (botQA !== null && botQA !== false) {
+        const obj: RhinoInferenceObject = {
+          botQA,
+          id: ExpoCrypto.getRandomBytes(16).toString(),
+        };
+        setHistory((previousState) => [...previousState, obj]);
+      } else if (botQA === false) {
+        killPicoVoice();
+      } else {
+        console.log('Inference Text is null');
+      }
+    };
+
+    const wakeWordCallback = () => {
+      console.log('wingler');
+      setIsListening(true);
+    };
+
     (async () => {
       try {
-        picovoiceManager = await PicovoiceManager.create(
+        picovoiceManager.current = await PicovoiceManager.create(
           ACCESS_KEY,
           'wingler_en_android_v2_2_0.ppn',
           wakeWordCallback,
@@ -60,16 +113,19 @@ const WinglerBot = () => {
         if (picovoiceManager === undefined) {
           throw new Error('picovoiceManager is undefined');
         } else {
-          await picovoiceManager.start();
-          setContextInfo(picovoiceManager.contextInfo as string);
-          // setPicoVoice(picovoiceManager);
+          await picovoiceManager.current.start();
+          setContextInfo(picovoiceManager.current.contextInfo as string);
         }
       } catch (error) {
         console.error('error', error);
       }
     })();
+
     return () => {
-      picovoiceManager?.stop();
+      Voice.cancel();
+      Voice.removeAllListeners();
+      Voice.destroy();
+      picovoiceManager?.current?.stop();
     };
   }, []);
 
@@ -78,6 +134,7 @@ const WinglerBot = () => {
       <View className="flex-1 justify-center bg-primary-dark">
         <ListenerIndicator
           buttonDisabled={isListening}
+          isSpeechToText={isSpeechToText}
           onPress={() => console.log('pressed')}
         />
 
