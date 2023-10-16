954aa0dbdcdd356b9d846e798efa702799a479c3
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-10-16 13:23:41 +0200

    feat(tailwind): moved wingler colors to commonJS module to be able to reference it

---
+++ src/styles/colors.js
@@ -0,0 +1,29 @@
+module.exports = {
+  primary: {
+    dark: '#151523',
+    black: '#090910',
+    DEFAULT: '#6668AB',
+  },
+  accent: {
+    DEFAULT: '#F1AC1E',
+    secondary: '#64FFD5',
+  },
+  secondary: '#64FFD5',
+  tertiary: '#F1AC1E',
+  quaternary: '#151523',
+  quinary: '#090910',
+  senary: '#6c6c6c',
+  septenary: '#7c7c7c',
+  octonary: '#8c8c8c',
+  nonary: '#9c9c9c',
+  denary: '#acacac',
+  eleventh: '#bcbcbc',
+  duodenary: '#cccccc',
+  tredecim: '#dcdcdc',
+  quattuordecim: '#ececec',
+  quindecim: '#fcfcfc',
+  inc: {
+    DEFAULT: '#FFFFFF',
+    spotify: '#1DB954',
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('./src/styles/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          dark: '#151523',
-          black: '#090910',
-          DEFAULT: '#6668AB',
-        },
-        accent: {
-          DEFAULT: '#F1AC1E',
-          secondary: '#64FFD5',
-        },
-        secondary: '#64FFD5',
-        tertiary: '#F1AC1E',
-        quaternary: '#151523',
-        quinary: '#090910',
-        senary: '#6c6c6c',
-        septenary: '#7c7c7c',
-        octonary: '#8c8c8c',
-        nonary: '#9c9c9c',
-        denary: '#acacac',
-        eleventh: '#bcbcbc',
-        duodenary: '#cccccc',
-        tredecim: '#dcdcdc',
-        quattuordecim: '#ececec',
-        quindecim: '#fcfcfc',
-      },
+      colors,
     },
   },
   plugins: [],
