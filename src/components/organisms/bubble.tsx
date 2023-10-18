b2d29b4903f6c5c71da8b1d68dce5c72158a6fec
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-10-18 12:25:37 +0200

    feat(chat): removed the wrap in favour of bubblewrap, also components are capitalised

--- src/components/organisms/bubble.tsx
+++ src/components/organisms/bubble.tsx
@@ -1,15 +1,16 @@
 import { Motion } from '@legendapp/motion';
-import { LinearGradient } from 'expo-linear-gradient';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 
-import Answer from '../molecules/answer';
-import AskAI from '../molecules/askAI';
-import Music from '../molecules/music';
-import User from '../molecules/user';
+import Answer from '../molecules/Answer';
+import AskAI from '../molecules/AskAI';
+import Music from '../molecules/Music';
+import Playback from '../molecules/PlayBack';
+import User from '../molecules/User';
 
 console.log();
 type BubbleProps = {
-  type: string;
+  // type: string;
+  type: keyof typeof bubbleMapping;
   content: object | string;
 };
 
@@ -17,23 +18,12 @@ type BubbleMapping = {
   [key: string]: React.FC<any>;
 };
 
-type ColorMapping = {
-  [key: string]: string;
-};
-
 const bubbleMapping: BubbleMapping = {
   user: User,
   answer: Answer,
   askAI: AskAI,
   music: Music,
-};
-
-const colorMapping: ColorMapping = {
-  user: 'bg-primary',
-  answer: 'bg-accent-secondary',
-  askAI: 'bg-senary',
-  music: 'bg-green-400',
-  // music: 'bg-inc-spotify',
+  playback: Playback,
 };
 
 const Content = ({ content, type }: BubbleProps) => {
@@ -43,38 +33,20 @@ const Content = ({ content, type }: BubbleProps) => {
   return <Component content={content} />;
 };
 
-const Bubble = ({ content, type }: BubbleProps) => {
-  let color = colorMapping[type];
-  if (!color) color = colorMapping.user;
-
-  return (
-    <Motion.View
-      className={`mx-4 flex-1 ${type === 'user' ? 'items-end' : 'items-start'}`}
-      initial={{ y: 50, opacity: 0 }}
-      animate={{
-        y: 0,
-        opacity: 1,
-      }}
-      whileHover={{ scale: 1.2 }}
-      whileTap={{ y: 20 }}
-      transition={{ type: 'spring', damping: 20 }}
-    >
-      <LinearGradient
-        colors={['#ffffff30', 'transparent', '#00000060']}
-        className={`m-4 flex-1 flex-row rounded-xl shadow-lg shadow-primary-black ${color}
-        ${type === 'user' ? 'rounded-tr-none' : 'rounded-tl-none'}
-        `}
-      >
-        <View
-          className={`rounded-xl border-[1px] border-[#ffffff15] px-6 py-4
-        ${type === 'user' ? 'rounded-tr-none' : 'rounded-tl-none'}
-        `}
-        >
-          <Content type={type} content={content} />
-        </View>
-      </LinearGradient>
-    </Motion.View>
-  );
-};
+const Bubble = ({ content, type }: BubbleProps) => (
+  <Motion.View
+    className={`mx-2 flex-1 ${type === 'user' ? 'items-end' : 'items-start'}`}
+    initial={{ y: 50, opacity: 0 }}
+    animate={{
+      y: 0,
+      opacity: 1,
+    }}
+    whileHover={{ scale: 1.2 }}
+    whileTap={{ y: 20 }}
+    transition={{ type: 'spring', damping: 20 }}
+  >
+    <Content type={type} content={content} />
+  </Motion.View>
+);
 
 export default Bubble;
