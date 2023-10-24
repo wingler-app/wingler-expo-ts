0effe012904d216394abf1497c6b83c7c9bc1524
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-10-24 15:05:21 +0200

    fix(hooks): handlequestion is now a cloudfunction hook

--- src/utils/handleQuestion.ts
+++
@@ -1,9 +0,0 @@
-import { firebase } from '@react-native-firebase/functions';
-
-export const promptOpenAI = async (question: string): Promise<string> => {
-  const { data } = await firebase.functions().httpsCallable('askAI')({
-    question,
-  });
-
-  return data.answer;
-};
