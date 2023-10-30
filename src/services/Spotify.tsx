820912658dc1aa82d9fa64aff97e736c8915c091
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-10-30 10:46:27 +0100

    feat(spotify): moving out all spotify api calls

--- src/services/Spotify.tsx
+++ src/services/Spotify.tsx
@@ -1,9 +1,43 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import type {
+  AuthRequest,
+  AuthRequestPromptOptions,
+  AuthSessionResult,
+} from 'expo-auth-session';
+import {
+  makeRedirectUri,
+  ResponseType,
+  useAuthRequest,
+} from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
 import { useEffect, useState } from 'react';
 import { getColors } from 'react-native-image-colors';
 import type { AndroidImageColors } from 'react-native-image-colors/build/types';
 
 const API: string = 'https://api.spotify.com/v1/';
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
+
+const scopes: string[] = [
+  'user-read-email',
+  'user-library-read',
+  'user-read-recently-played',
+  'user-top-read',
+  'user-read-playback-state',
+  'playlist-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'streaming',
+];
+
+type DeviceType = {
+  id: number;
+  name: string;
+};
 
 type Track = {
   uri: string;
@@ -117,8 +151,20 @@ const useArtist = (id: ArtistProps): ArtistResponse => {
 
 const useDevice = () => {
   const [loading, setLoading] = useState<boolean>(true);
-  const [answer, setAnswer] = useState<any>(null);
+  const [devices, setDevices] = useState<DeviceType[]>([]);
   const [error, setError] = useState<any>(null);
+  const [device, setDevice] = useState<DeviceType | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const d = await AsyncStorage.getItem('@SpotifyDevice');
+        if (d) setDevice(JSON.parse(d));
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, [device]);
 
   useEffect(() => {
     (async () => {
@@ -134,7 +180,10 @@ const useDevice = () => {
           },
         );
         const data = await response.json();
-        await setAnswer(data);
+        //  {"error": {"message": "The access token expired", "status": 401}}gg
+        if (data.error) throw new Error(data.error.message);
+        if (data.devices.length === 0) throw new Error('No devices found');
+        await setDevices(data.devices);
         setLoading(false);
       } catch (e) {
         setError(e);
@@ -142,7 +191,7 @@ const useDevice = () => {
     })();
   }, []);
 
-  return { answer, loading, error };
+  return { device, devices, loading, error };
 };
 
 const play = async (uri: string) => {
@@ -158,20 +207,72 @@ const play = async (uri: string) => {
 
   try {
     const accessToken = await AsyncStorage.getItem('@SpotifyToken');
-    await fetch(
-      'https://api.spotify.com/v1/me/player/play?device_id=74ae9cb15eda31ca459bf6e31757557861352fd1',
-      {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json',
-        },
-        body,
+    let device: DeviceType = { id: 0, name: '' };
+    const d = await AsyncStorage.getItem('@SpotifyDevice');
+    if (d) device = JSON.parse(d);
+    const url = `${API}me/player/play?device_id=${device.id}`;
+    console.log(url);
+    await fetch(url, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
       },
-    );
+      body,
+    });
   } catch (e) {
     console.error(e);
   }
 };
 
-export { play, useArtist, useDevice, useGenre };
+const useToken = (): [
+  AuthRequest | null,
+  AuthSessionResult | null,
+  (options?: AuthRequestPromptOptions) => Promise<AuthSessionResult>,
+  string,
+] => {
+  WebBrowser.maybeCompleteAuthSession();
+  const [token, setToken] = useState<string>('nothing');
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: '6960973e424a4929845ac3b16e377c68',
+      scopes,
+      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      redirectUri: makeRedirectUri({
+        native: 'com.wingler:/settings',
+      }),
+    },
+    discovery,
+  );
+
+  useEffect(() => {
+    (async () => {
+      const accessToken = await AsyncStorage.getItem('@SpotifyToken');
+      if (accessToken) setToken(accessToken);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      (async () => {
+        try {
+          // eslint-disable-next-line
+          let { access_token } = response.params;
+          access_token =
+            typeof access_token === 'string' ? access_token : 'nothing';
+          await setToken(access_token);
+          await AsyncStorage.setItem('@SpotifyToken', access_token);
+        } catch (e) {
+          console.log(e);
+        }
+      })();
+    }
+  }, [response]);
+
+  return [request, response, promptAsync, token];
+};
+
+export { play, useArtist, useDevice, useGenre, useToken };
