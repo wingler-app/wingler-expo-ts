228008fdec8b0d3f09136201bc682a204d54932f
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-10-31 11:23:47 +0100

    feat(spotify): use the new usePlayback hook and pause the music

--- src/components/molecules/PlayBack.tsx
+++ src/components/molecules/PlayBack.tsx
@@ -1,5 +1,7 @@
 import * as Speech from 'expo-speech';
 
+import { usePlayback } from '@/services/Spotify';
+
 import BubbleText from '../atoms/BubbleText';
 import BubbleWrap from '../atoms/BubbleWrap';
 
@@ -7,13 +9,18 @@ interface PlaybackProps {
   content: { command: string };
 }
 
-const playBack = (playback: string): void => {
-  if (playback === 'stop') Speech.stop();
-  console.log(playback);
-};
-
 const Playback = ({ content: { command } }: PlaybackProps) => {
+  const [, pause] = usePlayback();
+  const playBack = (playback: string): void => {
+    if (playback === 'stop') {
+      Speech.stop();
+      if (pause) pause();
+    }
+    console.log(playback);
+  };
+
   playBack(command);
+
   return (
     <BubbleWrap type="answer">
       <BubbleText dark>Ok!</BubbleText>
