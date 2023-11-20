8770e1b810f2cfaaf395817862d2c582bff8342f
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-11-20 10:10:28 +0100

    feat(spotify): useDevice is not a standard component SelectDevice

--- src/components/templates/Settings.tsx
+++ src/components/templates/Settings.tsx
@@ -1,19 +1,15 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import auth from '@react-native-firebase/auth';
 import { router } from 'expo-router';
-import { useState } from 'react';
 import { Text, View } from 'react-native';
 
-import { useDevice, useToken } from '@/services/Spotify';
+import SelectDevice from '@/components/organisms/SelectDevice';
+import useToken from '@/hooks/spotify/useToken';
 
 import Button from '../atoms/Button';
 import DevHelper from '../organisms/DevHelper';
-import WingModal from '../organisms/Modal';
 
 export default function SettingsTemplate() {
-  const [params, request, promptAsync] = useToken();
-  const [device, devices, loading, error] = useDevice();
-  const [showDevices, setShowDevices] = useState(false);
+  const [params, request, promptAsync, , handleToken] = useToken();
 
   const handleLogout = async () => {
     auth()
@@ -24,19 +20,6 @@ export default function SettingsTemplate() {
       });
   };
 
-  const handleSaveDevice = async (deviceObj: { id: number; name: string }) => {
-    try {
-      await AsyncStorage.setItem('@SpotifyDevice', JSON.stringify(deviceObj));
-      setShowDevices(false);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  if (!params) return <Text>waiting for params</Text>;
-  if (loading) return <Text>loading...</Text>;
-  if (error) return <Text>{error.message}</Text>;
-
   return (
     <View className="flex-1 items-center justify-center bg-primary-dark">
       <Button
@@ -44,35 +27,21 @@ export default function SettingsTemplate() {
         title="Get Spotify Token"
         onPress={() => promptAsync()}
       />
-      <Text className="text-white">{device?.name}</Text>
-      <Button
-        title="Select Spotify Device"
-        onPress={() => setShowDevices(true)}
-      />
+      {params?.refresh_token && (
+        <>
+          <Button
+            disabled={!request}
+            title="Refresh Spotify Token"
+            onPress={() => handleToken('refresh_token', params.refresh_token)}
+          />
+          <SelectDevice />
 
-      <Text className="m-4 rounded border-2 border-white p-4 text-white">
-        {params.access_token}
-      </Text>
-
-      <Button title="Logout" onPress={handleLogout} />
-      {devices && (
-        <WingModal
-          title="Devices"
-          visible={showDevices}
-          onClose={() => setShowDevices(false)}
-        >
-          <View className="">
-            {devices.map(({ id, name }) => (
-              <View key={id}>
-                <Button
-                  title={name}
-                  onPress={() => handleSaveDevice({ id, name })}
-                />
-              </View>
-            ))}
-          </View>
-        </WingModal>
+          <Text className="m-4 rounded border-2 border-white p-4 text-white">
+            {params.access_token}
+          </Text>
+        </>
       )}
+      <Button title="Logout" onPress={handleLogout} />
       <DevHelper />
     </View>
   );
