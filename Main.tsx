f0627add81e4eb974b5ed954ae3061e894f3a556
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-12-01 10:19:11 +0100

    refactor(backgroundcolor): moved to main

--- Main.tsx
+++ Main.tsx
@@ -1,5 +1,7 @@
 import 'expo-router/entry';
 
+import * as SystemUI from 'expo-system-ui';
 import { LogBox } from 'react-native';
 
+SystemUI.setBackgroundColorAsync('#151523');
 LogBox.ignoreLogs(['new NativeEventEmitter()']);
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,7 @@
 import { Stack } from 'expo-router';
-import * as SystemUI from 'expo-system-ui';
 
 import Wingler from '@/components/organisms/Wingler';
 
-SystemUI.setBackgroundColorAsync('#151523');
 export default function Layout() {
   return (
     <>
