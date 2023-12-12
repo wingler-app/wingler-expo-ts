d0607695cbfcf28d17f08a69b8f4b42f5740ccca
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-12-12 17:32:14 +0100

    feat(atom): button now has optional icons

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,20 +1,32 @@
+import { Ionicons } from '@expo/vector-icons';
 import { styled } from 'nativewind';
 import { useState } from 'react';
 import type { TextProps } from 'react-native';
 import { Pressable, Text } from 'react-native';
 
-type ButtonProps = {
+interface CommonProps {
   onPress: () => void;
-  title: string;
   buttonStyle?: TextProps['style'];
   textStyle?: TextProps['style'];
   disabled?: boolean;
   type?: keyof typeof buttonTypeMap;
-};
+  iconAfter?: boolean;
+}
+
+interface IconProps extends CommonProps {
+  icon: keyof typeof Ionicons.glyphMap;
+}
+
+interface TitleProps extends CommonProps {
+  title: string;
+}
+
+type ButtonProps = IconProps | TitleProps;
 
 const buttonTypeMap = {
   default: {
-    baseStyle: 'mb-6 rounded-full bg-accent px-4 py-2 w-auto self-center',
+    baseStyle:
+      'flex flex-row mb-6 rounded-full bg-accent px-4 py-2 w-auto self-center',
     pressedStyle: 'bg-accent-secondary',
     pressedTextStyle: '',
     disabledStyle: 'bg-gray-400',
@@ -23,29 +35,40 @@ const buttonTypeMap = {
   },
   menu: {
     baseStyle:
-      'flex px-6 py-4 bg-primary-dark border-2 border-primary-black mx-4 my-1 rounded-lg ',
+      'flex flex-row px-6 py-4 bg-primary-dark border-2 border-primary-black mx-4 my-1 rounded-lg ',
     pressedStyle: 'bg-primary',
     pressedTextStyle: 'text-primary-black opacity-100',
     disabledStyle: 'bg-gray-400',
     textStyle: 'text-center tracking-widest text-white dark:text-primary',
   },
   list: {
-    baseStyle: 'flex py-4 border-2 rounded-md border-primary-black mx-4 mb-2 ',
+    baseStyle:
+      'flex flex-row py-4 border-2 rounded-md border-primary-black mx-4 mb-2 ',
     pressedStyle: 'bg-primary-black',
     pressedTextStyle: '',
     disabledStyle: 'bg-gray-400',
     textStyle: 'px-4 tracking-widest text-white dark:text-secondary',
   },
+  minimal: {
+    baseStyle: 'flex mb-6 rounded-full px-4 bg-gray-100 flex-row py-2 w-auto',
+    pressedStyle: '',
+    pressedTextStyle: '',
+    disabledStyle: '',
+    textStyle: 'text-center tracking-widest text-black dark:text-primary',
+  },
 };
 
 function Button({
   onPress,
-  title,
   buttonStyle,
   textStyle,
   disabled,
   type,
+  iconAfter,
+  ...rest
 }: ButtonProps) {
+  const { icon, title } = rest as IconProps & TitleProps;
+
   const [isPressed, setIsPressed] = useState<boolean>(false);
 
   const styles = buttonTypeMap[type || 'default'];
@@ -66,14 +89,25 @@ function Button({
         ${disabled && styles.disabledStyle} ${buttonStyle}`}
       onPress={handleOnPress}
     >
+      {icon && !iconAfter && (
+        <Ionicons
+          className="bg-red-600 p-3"
+          name={icon}
+          size={26}
+          color="black"
+        />
+      )}
       <Text
         style={textStyle}
-        className={`${styles.textStyle} ${
-          isPressed && styles.pressedTextStyle
-        }`}
+        className={`${styles.textStyle}
+        ${isPressed && styles.pressedTextStyle}
+        ${icon && !iconAfter && title && 'ml-2'}
+        ${icon && iconAfter && title && 'mr-2'}
+        `}
       >
-        {title}
+        {title && title}
       </Text>
+      {icon && iconAfter && <Ionicons name={icon} size={24} color="black" />}
     </Pressable>
   );
 }
