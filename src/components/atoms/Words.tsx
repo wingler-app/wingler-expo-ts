07b2bb39cc82b9cb45a6b46c424535410c4743ce
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2023-12-18 15:22:40 +0100

    feat(words): h1 is now h
    H is a header component that now takes an optional size param, so does P

--- src/components/atoms/Words.tsx
+++ src/components/atoms/Words.tsx
@@ -6,13 +6,26 @@ interface WordsProps {
   className?: string;
   style?: StyleProp<TextStyle>;
   dark?: boolean;
+  size?: keyof typeof sizeMap;
 }
 
-const H1 = ({ children, className, style, dark }: WordsProps) => {
+const sizeMap = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  md: 'text-md',
+  lg: 'text-lg',
+  xl: 'text-xl',
+  '2xl': 'text-2xl',
+  '3xl': 'text-3xl',
+};
+
+const H = ({ children, className, style, dark, size }: WordsProps) => {
+  const sizeClass = size ? sizeMap[size] : 'text-4xl';
+
   return (
     <Text
       style={style}
-      className={`text-center text-4xl ${
+      className={`text-center ${sizeClass} ${
         dark ? 'text-accent-secondary' : 'text-accent'
       } ${className}`}
     >
@@ -21,15 +34,19 @@ const H1 = ({ children, className, style, dark }: WordsProps) => {
   );
 };
 
-const P = ({ children, className, style, dark }: WordsProps) => {
+const P = ({ children, className, style, dark, size }: WordsProps) => {
+  const sizeClass = size ? sizeMap[size] : 'text-xl';
+
   return (
     <Text
       style={style}
-      className={` text-center text-xl ${dark && 'text-white'} ${className}`}
+      className={` text-center ${sizeClass} ${
+        dark && 'text-white'
+      } ${className}`}
     >
       {children}
     </Text>
   );
 };
 
-export { H1, P };
+export { H, P };
