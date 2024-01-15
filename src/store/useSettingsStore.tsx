56b2ac4cf6fb1a54e8b5833f1bb786993925c692
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2024-01-15 13:36:52 +0100

    feat(settings): added showTextChat
    This setting and toggle functions show/hides the TextChat component in Chat.tsx

--- src/store/useSettingsStore.tsx
+++ src/store/useSettingsStore.tsx
@@ -4,16 +4,23 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface SettingsStoreState {
   readAloud: boolean;
+  showTextChat: boolean;
   toggleReadAloud: () => void;
+  toggleShowTextChat: () => void;
 }
 
 const useSettingsStore = create(
   persist<SettingsStoreState>(
     (set) => ({
       readAloud: false,
+      showTextChat: false,
       toggleReadAloud: () => {
         console.log('toggleReadAloud');
         return set((state) => ({ readAloud: !state.readAloud }));
+      },
+      toggleShowTextChat: () => {
+        console.log('toggleShowTextChat');
+        return set((state) => ({ showTextChat: !state.showTextChat }));
       },
     }),
     {
