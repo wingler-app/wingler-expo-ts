0e4779245e333af7437e1aef5b5d145d4e81dabe
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2024-01-24 13:00:37 +0100

    feat: add support for svg file imports
    To not get an error from typescript we declare a svg module type. More info here: https://dev.to/vinipachecov/how-to-use-svg-files-in-react-native-with-typescript-1bn7

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,20 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-// config.resolver.assetExts.push('db');
-module.exports = config;
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  };
+
+  return config;
+})();
--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="nativewind/types" />
+declare module '*.svg' {
+  import type { SvgProps } from 'react-native-svg';
+
+  const content: React.FC<SvgProps>;
+  export default content;
+}
